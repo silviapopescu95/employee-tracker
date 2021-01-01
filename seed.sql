1d92017061d9b60b5e56999761e02cb53ebf9bfd
Silvia Popescu <amazingaivlis@gmail.com>
2021-01-01 12:58:46 -0800

    Moved db files to db directory, made changes to package.json and package-lock.json.

--- seed.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO department (name) VALUES ("Software");
-
-INSERT INTO role (title, salary) VALUES ("Enginner", 40.00);
-
-INSERT INTO employee (first_name, last_name) VALUES ("Silvia", "Popescu");
