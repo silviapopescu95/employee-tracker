a54b2c38a86568a93628d65c88615335680abf15
Silvia Popescu <amazingaivlis@gmail.com>
2021-01-02 00:57:54 -0800

    Added employee data to seed.sql.

--- db/seed.sql
+++ db/seed.sql
@@ -13,4 +13,12 @@ INSERT INTO roles (title, salary, department_id) VALUES ("CEO", 250000, 3);
 INSERT INTO roles (title, salary, department_id) VALUES ("Software Manager", 200000, 3);
 INSERT INTO roles (title, salary, department_id) VALUES ("Sales Manager", 175000, 3);
 
-INSERT INTO employee (first_name, last_name, role_id) VALUES ("Silvia", "Popescu");
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES 
+("Silvia", "Popescu", 1, 2),
+("Alanna", "Griffin", 2, 2),
+("Daniel", "Davis", 3, 2),
+("Shad", "Smart", 4, 3),
+("Bryson", "Fox", 5, 3),
+("Quinn", "Randall", 6, NULL),
+("Eryk", "Burton", 7, NULL),
+("Justin", "Fiorino", 8, NULL);
