9264fa3a5edff6a58d42c7ff4a0b2f6c80adbe5c
Silvia Popescu <amazingaivlis@gmail.com>
2021-01-02 02:46:17 -0800

    Committing all changes because my laptop keeps crashing.

--- db/employeeTracker.js
+++ db/employeeTracker.js
@@ -1,5 +1,7 @@
 var mysql = require("mysql");
-var inquirer = require("inquirer");
+const inquirer = require("inquirer");
+const cTable = require("console.table");
+const util = require("util");
 
 // create the connection information for the sql database
 var connection = mysql.createConnection({
@@ -16,6 +18,8 @@ var connection = mysql.createConnection({
   database: "employeeTracker_DB"
 });
 
+connection.query = util.promisify(connection.query);
+
 // connect to mysql server and database
 connection.connect(function(err) {
     if (err) throw err;
@@ -23,101 +27,172 @@ connection.connect(function(err) {
     start()
 });
 
-// prompts user to add departments, roles, and employees
-function start() {
-  inquirer
-    .prompt([
-      {
-        name: "department",
-        type: "input",
-        message: "Please enter a Department name."
-      }
-    ])
-    .then(function(answer) {
-      // Insert a new department into the db
-      connection.query(
-        "INSERT INTO department SET ?",
-        {
-          name: answer.department
-        },
-        function(err) {
-          if (err) throw err;
-          console.log("You successfully added a department name.");
-          // now prompt user to add a role
-          inputRole();
-        }
-      );
-    });
+// initial prompt for user
+async function start() {
+  const { initialPrompt } = await inquirer.prompt({
+    type: "list",
+    name: "initialPrompt",
+    message: "Welcome to the Employee Tracker! Please choose how you would like to proceed below.",
+    choices: [
+      "View all departments",
+      "View all roles",
+      "View all employees",
+      "Add a new department",
+      "Add a new role",
+      "Add a new employee",
+      "Update an employee role"
+    ],
+  });
+  switch (initialPrompt) {
+    case "View all departments":
+      await viewDepartments();
+      break;
+    case "View all roles":
+      await viewRoles();  
+      break;
+    case "View all employees":
+      await viewEmployees();
+      break;
+    case "Add a new department":
+      await addDepartment();
+      break;
+    case "Add a new role":
+      await addRole();
+      break;
+    case "Add a new employee":
+      await addEmployee();
+      break;   
+    case "Update an employee role":
+      await updateRole();
+      break;
+    default: 
+      connection.end();    
+  }
 }
 
-// prompts user to add roles
-function inputRole() {
-    inquirer
-      .prompt([
-        {
-          name: "role",
-          type: "input",
-          message: "Please enter an Employee role."
-        },
-        {
-          name: "salary",
-          type: "input",
-          message: "Please enter the Employee's salary.",
-          validate: function(value) {
-            if (isNaN(value) === false) {
-              return true;  
-            }
-            return false;
-          }
-        }
-      ])
-      .then(function(answer) {
-        // Insert a new department into the db
-        connection.query(
-          "INSERT INTO role SET ?",
-          {
-            title: answer.role,
-            salary: answer.salary
-          },
-          function(err) {
-            if (err) throw err;
-            console.log("You successfully added a role.");
-            // now prompt user to add an employee
-            inputEmployee();
-          }
-        );
-      });
-  }
+async function viewDepartments() {
 
-  // prompts user to add employees
-function inputEmployee() {
-    inquirer
-      .prompt([
-        {
-          name: "firstName",
-          type: "input",
-          message: "Please enter Employee's first name."
-        },
-        {
-          name: "lastName",
-          type: "input",
-          message: "Please enter Employee's first name."
-        }
-      ])
-      .then(function(answer) {
-        // Insert a new department into the db
-        connection.query(
-        "INSERT INTO employee SET ?",
-        {
-          first_name: answer.firstName,
-          last_name: answer.lastName
-        },
-        function(err) {
-          if (err) throw err;
-          console.log("You successfully added an employee.");
-          // now prompt user to add another department, role, employee again
-          start();
-        }
-      );
-      });
-  }
+}
+
+async function viewRoles() {
+  
+}
+
+async function viewEmployees() {
+  
+}
+
+async function addDepartment() {
+  
+}
+
+async function addRole() {
+  
+}
+
+async function addEmployee() {
+  
+}
+
+async function updateRole() {
+  
+}
+
+// // prompts user to add departments, roles, and employees
+// function start() {
+//   inquirer
+//     .prompt([
+//       {
+//         name: "department",
+//         type: "input",
+//         message: "Please enter a Department name."
+//       }
+//     ])
+//     .then(function(answer) {
+//       // Insert a new department into the db
+//       connection.query(
+//         "INSERT INTO department SET ?",
+//         {
+//           name: answer.department
+//         },
+//         function(err) {
+//           if (err) throw err;
+//           console.log("You successfully added a department name.");
+//           // now prompt user to add a role
+//           inputRole();
+//         }
+//       );
+//     });
+// }
+
+// // prompts user to add roles
+// function inputRole() {
+//     inquirer
+//       .prompt([
+//         {
+//           name: "role",
+//           type: "input",
+//           message: "Please enter an Employee role."
+//         },
+//         {
+//           name: "salary",
+//           type: "input",
+//           message: "Please enter the Employee's salary.",
+//           validate: function(value) {
+//             if (isNaN(value) === false) {
+//               return true;  
+//             }
+//             return false;
+//           }
+//         }
+//       ])
+//       .then(function(answer) {
+//         // Insert a new department into the db
+//         connection.query(
+//           "INSERT INTO role SET ?",
+//           {
+//             title: answer.role,
+//             salary: answer.salary
+//           },
+//           function(err) {
+//             if (err) throw err;
+//             console.log("You successfully added a role.");
+//             // now prompt user to add an employee
+//             inputEmployee();
+//           }
+//         );
+//       });
+//   }
+
+//   // prompts user to add employees
+// function inputEmployee() {
+//     inquirer
+//       .prompt([
+//         {
+//           name: "firstName",
+//           type: "input",
+//           message: "Please enter Employee's first name."
+//         },
+//         {
+//           name: "lastName",
+//           type: "input",
+//           message: "Please enter Employee's first name."
+//         }
+//       ])
+//       .then(function(answer) {
+//         // Insert a new department into the db
+//         connection.query(
+//         "INSERT INTO employee SET ?",
+//         {
+//           first_name: answer.firstName,
+//           last_name: answer.lastName
+//         },
+//         function(err) {
+//           if (err) throw err;
+//           console.log("You successfully added an employee.");
+//           // now prompt user to add another department, role, employee again
+//           start();
+//         }
+//       );
+//       });
+//   }
--- db/employeeTrackerSchema.sql
+++ db/employeeTrackerSchema.sql
@@ -9,7 +9,7 @@ CREATE TABLE department(
     PRIMARY KEY (id)
 );
 
-CREATE TABLE role( 
+CREATE TABLE roles( 
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10,2) NOT NULL,
